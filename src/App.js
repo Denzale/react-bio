972b6f798ff1aa4d47838a529b6e429ab5b16d5e
Denzale Houston <denzalethouston@gmail.com>
2021-01-15 17:26:46 -0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Portfolio from './pages/Portfolio.js';
 import Contact from './pages/Contact.js';
 import About from './pages/About.js';
+import Navbar from './componets/Navbar.js';
+
 
 function App() {
   return (
-    
-    <Router>
+
+
+
+
+
+
+
+
+
+
+
+
+ <Router>
+      <Navbar></Navbar>
+
       <div className="App">
         <Route exact path="/portfolio" component={Portfolio} />
         <Route path="/contact" component={Contact} />
